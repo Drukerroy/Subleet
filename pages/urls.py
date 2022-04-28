c321d95674f07d8e766c54a804a00442962a0ae3
Drukerroy <roy.druker95@gmail.com>
2022-04-28 11:42:08 +0300

    terms and conditions

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('terms_and_conditions', views.terms_and_conditions,
+         name='terms_and_conditions')
 ]
